d9bb69d65f673e9c60baa7f5fa45f208f5cbad11
Vinicius Rodrigues <jairrodriguesmd@gmail.com>
2021-06-23 19:59:58 -0300

    new controls day03

--- main.js
+++ main.js
@@ -1,33 +1,64 @@
 // abre e fecha o menu onclick
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
-
-for(const element of toggle){
-   element.addEventListener("click",function(){
-     nav.classList.toggle("show")
-   })
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
 
 //fechar menu onclick nos links
-const links = document.querySelectorAll("nav ul li a");
-for(const link of links){
-  link.addEventListener("click",function(){
-    nav.classList.remove("show");
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
 
 /*Mudar o Header da página quando dermos scroll */
 
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
-window.addEventListener("scroll",function(){
-  if(window.scrollY >= navHeight){
-  //scroll é maior que a altura do header
-    header.classList.add("scroll");
-  }else{
-  //scroll é menor que a altura do header
-  header.classList.remove("scroll");
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //scroll é menor que a altura do header
+    header.classList.remove('scroll')
   }
 })
+
+//Testimonials carousel slider swipe
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+//Scroll Reveal: Mostrar elementos quando dermos scroll
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+contact .txt, #contact .links
+
+`,
+  { interval: 100 }
+)
